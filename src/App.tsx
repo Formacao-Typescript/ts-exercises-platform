fac6dfcc74b11cf4333d5471ff577458daae1151
WellsSA <wel.cavzod@gmail.com>
2023-12-17 10:59:47 -0300

    feat(src/App): react-toastify container setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import { ToastContainer } from 'react-toastify';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes />
+      <ToastContainer
+        autoClose={5000}
+        closeOnClick
+        draggable
+        hideProgressBar={false}
+        newestOnTop={false}
+        pauseOnFocusLoss={false}
+        pauseOnHover
+        position="bottom-right"
+        rtl={false}
+      />
     </Router>
   );
 };
