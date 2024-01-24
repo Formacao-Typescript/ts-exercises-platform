61f6eff1ea9ee5d12daadd126838dc0bb325e660
WellsSA <wel.cavzod@gmail.com>
2024-01-24 09:31:24 -0300

    feat(vite.config): md import resolver and aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
+import * as path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -19,4 +21,11 @@ export default defineConfig({
       ],
     }),
   ],
+  assetsInclude: ['**/*.md'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '#root': path.resolve(__dirname),
+    },
+  },
 });
