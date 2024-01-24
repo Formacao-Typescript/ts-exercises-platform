d863a660cd90e5cbd1fce9d3cbab2f6642f1f219
WellsSA <wel.cavzod@gmail.com>
2024-01-24 09:31:57 -0300

    feat(pages/Dashboard): rendering and alias updates

--- src/pages/public/Dashboard/index.tsx
+++ src/pages/public/Dashboard/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 // import MonacoMockup from '../../../assets/monaco-mockup.png';
 import { Container } from './styles';
 
-import MonacoEditor from '../../../components/MonacoEditor';
+import MonacoEditor from '@/components/MonacoEditor';
+import ExerciseRenderer from '@/components/ExerciseRenderer';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
       <h1>Dashboard</h1>
       <MonacoEditor code="console.log('banana')" />
+      <ExerciseRenderer source="exercises/SAMPLE.md"></ExerciseRenderer>
     </Container>
   );
 };
