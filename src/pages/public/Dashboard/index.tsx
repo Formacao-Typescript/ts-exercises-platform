851bda38d04b086a5561eb1faaea49b1a5c16b8f
WellsSA <wel.cavzod@gmail.com>
2024-02-06 23:36:11 -0300

    feat(public/Dashboard): JourneyCard implementation

--- src/pages/public/Dashboard/index.tsx
+++ src/pages/public/Dashboard/index.tsx
@@ -1,43 +1,21 @@
 import React from 'react';
 // import MonacoMockup from '../../../assets/monaco-mockup.png';
 
-import { Card, Progress } from 'flowbite-react';
 import MOCK_DATA from '@/assets/data.mock';
+import JourneyCard from '@/components/journey/JourneyCard';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div className="p-4 bg-gray-600 min-h-full">
       <div className="grid grid-cols-3 gap-4">
-        {MOCK_DATA.map((journey, index) => (
-          <div key={journey.id} className="col-span-1">
-            <Card
-              className="max-w-sm"
-              renderImage={() => (
-                <img
-                  src="https://placehold.co/500x500"
-                  width={500}
-                  height={500}
-                  alt="image 1"
-                />
-              )}
-            >
-              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {journey.name}
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                {journey.longDescription}
-              </p>
-              <Progress
-                progress={index * 25}
-                progressLabelPosition="inside"
-                textLabel="Progresso"
-                textLabelPosition="outside"
-                size="lg"
-                labelProgress
-                labelText
-              />
-            </Card>
-          </div>
+        {MOCK_DATA.map(journey => (
+          <JourneyCard
+            key={journey.id}
+            journey={journey}
+            onClick={() => navigate('/journey/' + journey.id)}
+          />
         ))}
       </div>
 
