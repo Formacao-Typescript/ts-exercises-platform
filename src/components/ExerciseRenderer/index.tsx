629e58b54119c355b80eedd43e33c0e0fc4d9838
WellsSA <wel.cavzod@gmail.com>
2024-02-19 20:52:18 -0300

    feat(components/ExerciseRenderer): responsive formatting

--- src/components/ExerciseRenderer/index.tsx
+++ src/components/ExerciseRenderer/index.tsx
@@ -25,7 +25,7 @@ const ExerciseRenderer: React.FC<ExerciseRendererProps> = ({ source }) => {
     void load(source);
   }, [source]);
   return (
-    <Container className="format-lg dark:format-invert">
+    <Container className="xs:format-sm md:format-lg dark:format-invert">
       <Markdown
         components={{
           code: node => {
