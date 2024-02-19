997c156bc9a80e5f62050190b84d47d4c99c8fcf
WellsSA <wel.cavzod@gmail.com>
2024-02-19 18:59:40 -0300

    feat(pages/Journey): Journey skeleton loading

--- src/pages/public/Journey/index.tsx
+++ src/pages/public/Journey/index.tsx
@@ -3,6 +3,8 @@ import { Progress } from 'flowbite-react';
 import { useNavigate, useParams, Outlet } from 'react-router-dom';
 import { ITopic } from '@/types';
 import { useJourney } from '@/hooks';
+import LoadSkeleton from '@/components/LoadSkeleton';
+import { AiOutlinePicture } from 'react-icons/ai';
 
 const Journey: React.FC = () => {
   const { journeyId, topicId } = useParams<{
@@ -14,14 +16,14 @@ const Journey: React.FC = () => {
 
   if (topicId) return <Outlet />;
 
-  if (isLoading) return <div>Loading...</div>;
+  // if (isLoading) return <div>Loading...</div>;
 
-  if (isEmpty || !journey)
-    return (
-      <div className="p-4 bg-gray-900 text-white">
-        <h1>Journey not found</h1>
-      </div>
-    );
+  // if (isEmpty || !journey)
+  //   return (
+  //     <div className="p-4 bg-gray-900 text-white">
+  //       <h1>Journey not found</h1>
+  //     </div>
+  //   );
 
   const actions = {
     navigateToTopic: (topic: ITopic) => {
@@ -34,27 +36,73 @@ const Journey: React.FC = () => {
   return (
     <>
       <section className="bg-white dark:bg-gray-900 h-full">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
-          <div className="text-left mb-8 lg:mb-16">
-            <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              {journey.name}
-            </h1>
-            <h1 className="mb-4 text-2xl tracking-tight  text-gray-900 dark:text-gray-400">
-              {journey.shortDescription}
-            </h1>
-            <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
-              {journey.longDescription}
-            </p>
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:px-6 ">
+          <div className="text-left mb-4">
+            <LoadSkeleton
+              isLoading={true}
+              skeleton={() => (
+                <div className="w-full pr-4">
+                  <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                  <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
+                  <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                  <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
+                  <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
+                  <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                </div>
+              )}
+            >
+              <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                {journey?.name}
+              </h1>
+              <h1 className="mb-4 text-2xl tracking-tight  text-gray-900 dark:text-gray-400">
+                {journey?.shortDescription}
+              </h1>
+              <p className="font-light text-gray-500 mb-8 sm:text-xl dark:text-gray-400">
+                {journey?.longDescription}
+              </p>
+            </LoadSkeleton>
           </div>
-          <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-4">
-            {[...journey.topics, ...journey.topics, ...journey.topics].map(
-              topic => (
+          <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-3">
+            <LoadSkeleton
+              isLoading={true}
+              skeleton={() => (
+                <>
+                  {Array(3)
+                    .fill(0)
+                    .map((_, i) => (
+                      <div
+                        key={'status' + i}
+                        role="status"
+                        className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse flex flex-col"
+                      >
+                        <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded dark:bg-gray-700 mb-10">
+                          <AiOutlinePicture
+                            size={32}
+                            className="text-gray-500"
+                          />
+                        </div>
+                        <div className="w-full pr-4">
+                          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
+                          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
+                          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
+                          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                        </div>
+                        <span className="sr-only">Loading...</span>
+                      </div>
+                    ))}
+                </>
+              )}
+            >
+              {(journey?.topics || []).map(topic => (
                 <div
+                  key={topic.id}
                   className="bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700 cursor-pointer flex flex-col"
                   onClick={actions.navigateToTopic.bind(null, topic)}
                 >
-                  <div className="rounded-lg max-w-sm h-300 bg-gradient-radial from-gray-600 to-gray-800 flex flex-col justify-center items-center">
-                    <span className="w-full p-4 text-5xl bg-gradient-to-l from-indigo-500 to-blue-600 inline-block text-transparent bg-clip-text break-words">
+                  <div className="rounded-lg w-full h-64 bg-gradient-radial from-gray-600 to-gray-800 flex flex-col justify-center items-center">
+                    <span className="w-full p-4 text-5xl bg-gradient-to-l from-trade-light-blue to-trade-blue inline-block text-transparent bg-clip-text break-words">
                       {topic.name}
                     </span>
                   </div>
@@ -76,8 +124,8 @@ const Journey: React.FC = () => {
                     />
                   </div>
                 </div>
-              )
-            )}
+              ))}
+            </LoadSkeleton>
           </div>
         </div>
       </section>
