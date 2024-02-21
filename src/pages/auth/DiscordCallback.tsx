6363c28fd0fbaf1bc9ff982f8ecd8002a1c12408
WellsSA <wel.cavzod@gmail.com>
2024-02-21 16:33:43 -0300

    feat(pages/auth): discord callback error handling

--- src/pages/auth/DiscordCallback.tsx
+++ src/pages/auth/DiscordCallback.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { useSearchParams } from '@/hooks';
+import React, { useEffect } from 'react';
 
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const DiscordCallback: React.FC = () => {
   const { search } = useLocation();
+  const navigate = useNavigate();
+  const searchParams = useSearchParams('error');
   console.log(search);
+
+  useEffect(() => {
+    if (searchParams.error) {
+      const params = new URLSearchParams({
+        error: searchParams.error,
+        platform: 'discord',
+      });
+      navigate(`/auth?${params}`);
+    }
+
+    if (searchParams.code) {
+      // TODO: use code to get info
+    }
+  }, [searchParams]);
 
   return (
     <div>
