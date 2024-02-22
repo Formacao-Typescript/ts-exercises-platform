ad2c4f8b190df9026cd65a81deb834b2b422bced
WellsSA <wel.cavzod@gmail.com>
2024-02-22 05:50:23 -0300

    Updates

--- assets/freemarker2-F5c6jx92.js
+++ assets/freemarker2-F5c6jx92.js
@@ -1,4 +1,4 @@
-import{m as F}from"./index-s1Fihof3.js";/*!-----------------------------------------------------------------------------
+import{m as F}from"./index-oSHRE49f.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
  * Released under the MIT license
