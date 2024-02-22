ad358bf90c058e6b2fe5803c66d98e717376e4b5
WellsSA <wel.cavzod@gmail.com>
2024-02-22 12:17:50 -0300

    feat(layouts/auth): adding build number

--- src/pages/_layouts/AuthLayout/index.tsx
+++ src/pages/_layouts/AuthLayout/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+const BUILD_NUMBER = import.meta.env.VITE_BUILD_NUMBER;
+
 const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div>
+      <h1>Build: {BUILD_NUMBER}</h1>
       <h1>aooo potencia</h1>
       {children}
       <Outlet />
