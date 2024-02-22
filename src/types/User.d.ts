34cf4640d0108f9cce69c7ac61361746af768a0c
WellsSA <wel.cavzod@gmail.com>
2024-02-22 12:42:07 -0300

    feat(types/User): setting up token info

---
+++ src/types/User.d.ts
@@ -0,0 +1,16 @@
+export interface IUserToken {
+  token_type: string;
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+}
+
+export interface IUser {
+  token: IUserToken;
+  progress: {
+    journeys: Record<IJourney[id], number>;
+    topics: Record<ITopic[id], number>;
+    activities: IActivity[id][];
+  };
+}
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,2 @@
 export * from './Exercises';
-
-// --- MOCK DATA --- //
-
-export interface IUser {
-  id: string;
-  name: string;
-  email: string;
-  progress: {
-    journeys: Record<IJourney[id], number>;
-    topics: Record<ITopic[id], number>;
-    activities: IActivity[id][];
-  };
-}
-
-// --- MOCK DATA --- //
+export * from './User';
