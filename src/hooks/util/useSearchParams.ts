289817f0f85c04c29e96366c87f55dbc1bc717d5
WellsSA <wel.cavzod@gmail.com>
2024-02-23 16:53:06 -0300

    feat(hooks/util): useSearchParams type updates

--- src/hooks/util/useSearchParams.ts
+++ src/hooks/util/useSearchParams.ts
@@ -1,17 +1,18 @@
 import React from 'react';
 
-type UseSearchParamsReturnType = Record<string, string | undefined>;
-
-const useSearchParams = (...params: string[]): UseSearchParamsReturnType => {
+const useSearchParams = (...params: string[]) => {
   const search = window.location.search;
 
   const query = React.useMemo(() => new URLSearchParams(search), [search]);
 
-  return params.reduce((acc, param) => {
-    const value = query.get(param);
-    acc[param] = value ? decodeURI(value).replace(' ', '+') : '';
-    return acc;
-  }, {} as UseSearchParamsReturnType);
+  return params.reduce(
+    (acc, param) => {
+      const value = query.get(param);
+      acc[param] = value ? decodeURI(value).replace(' ', '+') : '';
+      return acc;
+    },
+    {} as Record<string, string>
+  );
 };
 
 export default useSearchParams;
