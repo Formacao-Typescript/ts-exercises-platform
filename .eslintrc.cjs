305c2fcfde3ffa785931eba3fd9a33d74481aa0f
WellsSA <wel.cavzod@gmail.com>
2024-02-24 11:22:06 -0300

    chore: updating ts rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
   },
   parserOptions: {
     ecmaVersion: 'latest',
