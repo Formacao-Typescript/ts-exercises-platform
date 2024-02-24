d2f1feea317abba0351cc0f0110b82f25195befa
WellsSA <wel.cavzod@gmail.com>
2024-02-24 00:02:58 -0300

    feat(pages/auth): add discord login button and global styles

--- src/pages/_layouts/AuthLayout/index.tsx
+++ src/pages/_layouts/AuthLayout/index.tsx
@@ -5,14 +5,16 @@ const BUILD_NUMBER = import.meta.env.VITE_BUILD_NUMBER;
 
 const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div>
-      <Link to="/">
-        <button type="button">return home</button>
-      </Link>
-      <h1>Build: {BUILD_NUMBER}</h1>
-      <h1>aooo potencia</h1>
-      {children}
-      <Outlet />
+    <div className="text-white w-full h-full flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-800 p-8 rounded-lg">
+        <Link to="/">
+          <button type="button">return home</button>
+        </Link>
+        <h1>Build: {BUILD_NUMBER}</h1>
+        <h1>aooo potencia</h1>
+        {children}
+        <Outlet />
+      </div>
     </div>
   );
 };
--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,5 +1,6 @@
 import { getAuthorizationCodeUrl } from '@/utils/discord';
 import React from 'react';
+import { FaDiscord as DiscordIcon } from 'react-icons/fa';
 
 const {
   VITE_DISCORD_OAUTH_API_URL: DISCORD_API_URL,
@@ -26,11 +27,13 @@ const AuthLayout: React.FC = () => {
       </pre>
       <button
         type="button"
+        className="text-white bg-[#5F69F3] hover:bg-[#5F69F3]/90 focus:ring-4 focus:outline-none focus:ring-[#5F69F3]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#5F69F3]/55 me-2 mb-2"
         onClick={() => {
           window.location.href = getAuthorizationCodeUrl();
         }}
       >
-        Discord SSO
+        <DiscordIcon className="w-4 h-4 me-2" />
+        Login com Discord
       </button>
     </div>
   );
