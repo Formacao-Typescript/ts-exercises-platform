da7a3be9ca7c72b03607951f4172a40697cc8420
WellsSA <wel.cavzod@gmail.com>
2024-02-24 09:35:25 -0300

    feat(shared/marketing): branding common info definition

---
+++ src/shared/marketing.ts
@@ -0,0 +1,31 @@
+import AppLogo from '@/assets/logo-formacaots-hotmart.webp';
+
+import {
+  BsFacebook as FacebookIcon,
+  BsInstagram as InstagramIcon,
+  BsTwitter as TwitterIcon,
+  BsYoutube as YoutubeIcon,
+  BsLinkedin as LinkedinIcon,
+  BsMailbox as MailIcon,
+} from 'react-icons/bs';
+
+const LINKS = {
+  formacao: 'https://formacaots.com.br/',
+  instagram: 'https://formacaots.com.br/instagram.lsantos.dev',
+  facebook: 'https://formacaots.com.br/facebook.lsantos.dev',
+  twitter: 'https://formacaots.com.br/twitter.lsantos.dev',
+  youtube: 'https://formacaots.com.br/youtube.lsantos.dev',
+  linkedin: 'https://formacaots.com.br/linkedin.lsantos.dev',
+  mail: 'mailto:hello@lsantos.dev',
+};
+
+export {
+  AppLogo,
+  LINKS,
+  FacebookIcon,
+  InstagramIcon,
+  TwitterIcon,
+  YoutubeIcon,
+  LinkedinIcon,
+  MailIcon,
+};
