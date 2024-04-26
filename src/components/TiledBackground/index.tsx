91d07d35b2c27068ea36249de59e4fe40c4b5b14
WellsSA <wel.cavzod@gmail.com>
2024-04-26 14:43:50 -0300

    feat(components/TiledBackground): background definition and refactoring

---
+++ src/components/TiledBackground/index.tsx
@@ -0,0 +1,136 @@
+import React, { useEffect } from 'react';
+import anime from 'animejs';
+import { Container } from './styles';
+import { TSFixMe } from '@/types';
+import { findClosestTile } from '@/utils/tilegrid';
+
+const TiledBackground: React.FC = () => {
+  useEffect(() => {
+    const TILE_SIZE = 50; // 50px X 50px
+    const wrapper = document.getElementById('tiles');
+
+    let toggled = false;
+    const handleOnClick = (index: number, columns: number, rows: number) => {
+      toggled = !toggled;
+      anime({
+        targets: '.tile',
+        easing: 'easeOutExpo',
+        // opacity: toggled ? 0 : 1,
+        // border: toggled ? '' : '1px solid #fff',
+        borderRadius: toggled ? ['0%', '50%'] : ['50%', '0%'],
+        scale: [
+          { value: 0.1, easing: 'easeOutSine', duration: 500 },
+          { value: 1, easing: 'easeInOutQuad', duration: 1200 },
+        ],
+        delay: anime.stagger(TILE_SIZE, {
+          grid: [columns, rows],
+          from: index,
+        }),
+      });
+    };
+
+    const createTile = (index: number, onClick: (index: number) => void) => {
+      const tile = document.createElement('div');
+      tile.classList.add('tile');
+
+      tile.onclick = () => onClick(index);
+
+      return tile;
+    };
+
+    const createTiles = (
+      elementWrapper: HTMLElement,
+      quantity: number,
+      onClick: (index: number) => void
+    ) => {
+      Array.from(Array(quantity)).map((_, index) => {
+        elementWrapper.appendChild(createTile(index, onClick));
+      });
+    };
+
+    const createGrid = (elementWrapper: HTMLElement, tileSize: TSFixMe) => {
+      elementWrapper.innerHTML = '';
+
+      const columns = Math.floor(window.innerWidth / tileSize);
+      const rows = Math.floor(window.innerHeight / tileSize);
+
+      elementWrapper.style.setProperty('--columns', String(columns));
+      elementWrapper.style.setProperty('--rows', String(rows));
+
+      document.getElementById('tiles')!.style.gridTemplateColumns =
+        `repeat(${columns}, 1fr)`;
+      document.getElementById('tiles')!.style.gridTemplateRows =
+        `repeat(${rows}, 1fr)`;
+
+      const onClick = (index: number) => handleOnClick(index, columns, rows);
+      createTiles(elementWrapper, columns * rows, onClick);
+      console.log(`Grid created with ${columns} columns and ${rows} rows`);
+    };
+
+    window.onresize = () => createGrid(wrapper!, TILE_SIZE);
+
+    createGrid(wrapper!, TILE_SIZE);
+
+    let clickLocked = false;
+
+    window.onclick = e => {
+      if (clickLocked) return;
+
+      clickLocked = true;
+      const tiles = document.querySelectorAll('.tile');
+      const closestTile = findClosestTile(tiles, e.clientX, e.clientY);
+      const columns = Math.floor(window.innerWidth / TILE_SIZE);
+      const rows = Math.floor(window.innerHeight / TILE_SIZE);
+
+      handleOnClick(closestTile.index, columns, rows);
+
+      const cursor = document.getElementById('cursor')!;
+
+      cursor.style.opacity = '0';
+
+      setTimeout(() => {
+        clickLocked = false;
+      }, 2500);
+
+      anime({
+        targets: [cursor],
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 1000,
+        delay: 2500,
+      });
+    };
+
+    window.onmouseover = () => {
+      document.getElementById('cursor')!.style.visibility = 'visible';
+    };
+    window.onmouseout = () => {
+      document.getElementById('cursor')!.style.visibility = 'hidden';
+    };
+    window.onmousemove = e => {
+      const cursor = document.getElementById('cursor')!;
+      cursor.style.top = e.pageY + 'px';
+      cursor.style.left = e.pageX + 'px';
+    };
+
+    // Dispatch fake event on load to trigger cool animation
+    window.dispatchEvent(
+      new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+  }, []);
+
+  return (
+    <Container>
+      <div className="tiled-background">
+        <div id="tiles"></div>
+        <div id="cursor"></div>
+      </div>
+    </Container>
+  );
+};
+
+export default TiledBackground;
---
+++ src/components/TiledBackground/styles.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  @keyframes background-pan {
+    from {
+      background-position: 0% center;
+    }
+    to {
+      background-position: -200% center;
+    }
+  }
+
+  .tiled-background {
+    position: fixed;
+    /* background: pink; */
+    width: 100%;
+    height: 100%;
+
+    animation: background-pan 10s linear infinite;
+    background: linear-gradient(
+      to right,
+      rgb(98, 0, 234),
+      rgb(236, 64, 122),
+      rgb(98, 0, 234)
+    );
+    background-size: 200%;
+    overflow: hidden;
+  }
+
+  #tiles {
+    height: 100%;
+    width: 100%;
+    display: grid;
+    /* grid-template-columns: repeat(var(--columns), 1fr);
+  grid-template-rows: repeat(var(--rows), 1fr); */
+    pointer-events: all;
+    z-index: 1900;
+  }
+
+  .tile {
+    position: relative;
+    z-index: 1;
+  }
+  .tile::before {
+    background-color: rgb(20, 20, 20);
+    content: '';
+    position: absolute;
+    inset: 0.5px;
+  }
+
+  #cursor {
+    position: fixed;
+    width: 500px;
+    height: 500px;
+    background: radial-gradient(lime, transparent, transparent);
+    border-radius: 50%;
+    transform: translate(-50%, -50%);
+    visibility: visible;
+
+    @media (max-width: 768px) {
+      visibility: hidden !important;
+    }
+  }
+`;
