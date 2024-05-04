cd93b24db87c99a595c16c09bb989f67f6ebffb1
WellsSA <wel.cavzod@gmail.com>
2024-05-04 02:13:43 -0300

    chore(styles): rudimentar branding css setup

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -17,6 +17,35 @@ const GlobalStyles = createGlobalStyle`
 
     // Toastify theme variables: https://fkhadra.github.io/react-toastify/how-to-style
     --toastify-color-dark: #1e2638; 
+
+  // FIXME: Figure out branding colors and CSS variables management later on
+  /* gradients */
+  --trade-expert-gradient: linear-gradient(45deg, var(--accent-orange), var(--accent-red));
+  --trade-intermediate-gradient: linear-gradient(45deg, var(--accent-pink), var(--accent-dark-purple));
+  --trade-essential-gradient: linear-gradient(45deg, var(--accent-teal), var(--accent-blue));
+  
+  /* LSantos branding */
+  --branding-green: #45bb8b;
+  --branding-blue: #197bc0;
+  --branding-red: #ed1c24;
+  --branding-yellow: #f6b21a;
+  
+  /* backgrounds */
+  --background: #000020;
+  --bg-dark-purple: #0a0020;
+  --bg-mid-purple: #100523;
+  --bg-purple: #180b2d;
+  --bg-black: #000016;
+  --background-gradient: linear-gradient(135deg, var(--bg-black) 80%, var(--accent-dark-blue));
+  /* other */
+  --accent-dark-blue: #006dfb;
+  --accent-teal: #00adca;
+  --accent-blue: #0066ff;
+  --accent-dark-purple: #6002fc;
+  --accent-pink: #bb03c9;
+  --accent-orange: #f85400;
+  --accent-redish: #dd2900;
+  --accent-red: #c80000;
   }
 
   html {
