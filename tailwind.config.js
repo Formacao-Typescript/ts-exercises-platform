b52ae1b5e66f8fcdcb05087585f3a4dd4183f4f1
WellsSA <wel.cavzod@gmail.com>
2024-05-14 12:58:24 -0300

    chore: flowbite config updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,7 +58,8 @@ export default {
   },
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite/**/*.js',
     'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
-  plugins: [require('flowbite/plugin'), require('flowbite-typography')],
+  plugins: [require('flowbite/plugin')],
 };
