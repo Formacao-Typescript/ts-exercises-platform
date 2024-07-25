1e05e0df398aab1be8ecf4a390f48a24cb40123f
WellsSA <wel.cavzod@gmail.com>
2024-07-25 17:02:05 -0300

    feat(services/discord): implementing api-integrated fetchUser

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,18 +1,5 @@
-import { IDiscordUser, IUserToken } from '@/types';
+import { IUser, IUserToken } from '@/types';
 import { get } from './api';
-const {
-  VITE_DISCORD_API_URL,
-  VITE_DISCORD_OAUTH_API_URL,
-  VITE_DISCORD_OAUTH_REDIRECT_URL,
-  VITE_DISCORD_OAUTH_CLIENT_ID,
-  VITE_DISCORD_OAUTH_CLIENT_SECRET,
-} = import.meta.env;
-
-const DISCORD_API_URL = VITE_DISCORD_API_URL as string;
-const DISCORD_OAUTH_API_URL = VITE_DISCORD_OAUTH_API_URL as string;
-const REDIRECT_URL = VITE_DISCORD_OAUTH_REDIRECT_URL as string;
-const CLIENT_ID = VITE_DISCORD_OAUTH_CLIENT_ID as string;
-const CLIENT_SECRET = VITE_DISCORD_OAUTH_CLIENT_SECRET as string;
 
 /**
  * Generates the URL to redirect the user to for Discord OAuth
@@ -28,51 +15,6 @@ export const getAuthorizationCodeUrl = async (): Promise<string> => {
 };
 
 /**
- * Exchanges the code received from the Discord OAuth redirect for a user token
- * @param code {string} - The code received from the Discord OAuth redirect
- * @returns {Promise<IUserToken>} - The user token
- */
-export const getToken = async (code: string): Promise<IUserToken> => {
-  const params = new URLSearchParams({
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: REDIRECT_URL,
-  });
-
-  const response = await fetch(DISCORD_OAUTH_API_URL + '/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
-    },
-    body: params.toString(),
-  });
-
-  const data = (await response.json()) as IUserToken;
-  if (!data.access_token) {
-    throw new Error('Token request failed');
-  }
-
-  return data;
-};
-
-/**
- * Fetches the user data from Discord
- * @param token {IUserToken} - The user token
- * @returns {Promise<any>} - The user data
- */
-export const getUser = async (token: IUserToken): Promise<IDiscordUser> => {
-  const response = await fetch(DISCORD_API_URL + '/users/@me', {
-    headers: {
-      Authorization: `${token.token_type} ${token.access_token}`,
-    },
-  });
-
-  const data = (await response.json()) as IDiscordUser;
-  return data;
-};
-
-/**
  * Fetches the user data from Discord
  * @param code {string} - The code received from the Discord OAuth redirect
  * @returns {Promise<{ user: any; token: IUserToken }>} - The user data and token
@@ -80,10 +22,18 @@ export const getUser = async (token: IUserToken): Promise<IDiscordUser> => {
  * @throws {Error} - If the user request fails
  * @throws {Error} - If the token or user data is missing
  */
-export const fetchUser = async (
-  code: string
-): Promise<{ user: IDiscordUser; token: IUserToken }> => {
-  const token = await getToken(code);
-  const user = await getUser(token);
-  return { user, token };
+export const fetchUser = async (code: string): Promise<IUser> => {
+  const params = new URLSearchParams({
+    code: code,
+  });
+
+  const url = '/public/auth/discord/token?' + params.toString();
+
+  const [error, response] = await get<{ user: IUser; token: IUserToken }>(url);
+
+  if (error) throw new Error(error);
+
+  response!.user.token = response!.token;
+
+  return response!.user;
 };
